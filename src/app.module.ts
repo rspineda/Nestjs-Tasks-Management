1ef057dce9b2513f9c2266c0513897877b8d9c22
Ronald <ronystivens@hotmail.com>
2021-03-19 12:46:16 +0100

    added tasks service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TaskController } from './task/task.controller';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
 
 @Module({
   imports: [TasksModule],
-  controllers: [TaskController],
+  controllers: [TasksController],
+  providers: [TasksService]
 })
 export class AppModule {}
