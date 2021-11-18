2cb45238811484d5e207f383a3c714ba5998f297
Ronald <ronystivens@hotmail.com>
2021-11-18 23:21:01 +0100

    validation when a specific task is not found

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import {v1 as uuid }from 'uuid';
@@ -33,8 +33,14 @@ export class TasksService {
     return tasks;
   }
 
-  getTaskById(id: string):Task {
-    return this.tasks.find(task => task.id === id);
+  getTaskById(id: string): Task {
+    const task = this.tasks.find(task => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+
+    return task;
   };
 
   createTask(createTaskDto: CreateTaskDto): Task {
