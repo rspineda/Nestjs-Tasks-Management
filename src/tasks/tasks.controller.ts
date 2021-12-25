e704d8011147f686ef30f645b3d56622a1467efa
Ronald <ronystivens@hotmail.com>
2021-12-25 14:51:07 +0100

    restricted updateTaskStatus to authenticated user

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -47,9 +47,10 @@ export class TasksController {
   updateTaskStatus(
   @Param('id') id: string,
   @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  @GetUser() user: User,
   ): Promise<Task> { 
     const { status } = updateTaskStatusDto;
-    return this.tasksService.updateTaskStatus(id, status);
+    return this.tasksService.updateTaskStatus(id, status, user);
   }
 }
 
